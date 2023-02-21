c509941dbe76dfed6e21d5fa24e80d9a6fb47af1
sheetal <sheetalshevalkar265@gmail.com>
2023-02-21 17:21:46 +0530

    final trial

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'dockerized app deployment done ';
+    return 'dockerized app deployment done for one web app ';
   }
 }
