5f8fef398a4412245316afd6c744270634de56a7
sheetal <sheetalshevalkar265@gmail.com>
2023-02-21 15:26:50 +0530

    test chane

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "dockerized app deployment done"', () => {
+      expect(appController.getHello()).toBe('dockerized app deployment done');
     });
   });
 });
